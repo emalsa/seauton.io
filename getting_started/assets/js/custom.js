dcbf7343d2a8e928ae24f8dbfbf5567390359a21
nicastro <setaloro@hotmail.com>
2019-03-10 10:58:05 +0100

    optimize image

--- getting_started/assets/js/custom.js
+++ getting_started/assets/js/custom.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     'use strict';
     //Intialize Slider
-    var slider = new Swiper('#hero', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        loop: true,
-        parallax: true,
-        speed:1000,
-    });
+    // var slider = new Swiper('#hero', {
+    //     pagination: '.swiper-pagination',
+    //     paginationClickable: true,
+    //     nextButton: '.swiper-button-next',
+    //     prevButton: '.swiper-button-prev',
+    //     slidesPerView: 1,
+    //     loop: true,
+    //     parallax: true,
+    //     speed:1000,
+    // });
 
    // Initialize Search
     $('[data-pages="search"]').search({
